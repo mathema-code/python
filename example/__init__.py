4d20844d6054b95cfacadcb145263101f4897f09
caiosalgado <caio.salgado@hotmail.com>
2021-10-29 21:59:43 -0300

    verificando se o arquivo existe

--- example/__init__.py
+++ example/__init__.py
@@ -2,5 +2,10 @@ import check50
 
 @check50.check()
 def exists():
-    """hello.py exists"""
-    check50.exists("hello.py")
+    """Verificando se existe o arquivo hello.py"""
+    check50.exists('hello.py')
+
+@check50.check(exists)
+def hello_world():
+    """imprimindo "hello world" \n"""
+    check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
