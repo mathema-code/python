d216be213ff58f10567ae1a6f88d3b020da85d78
caiosalgado <caio.salgado@hotmail.com>
2021-10-29 22:22:10 -0300

    hello refeito

--- example/__init__.py
+++ example/__init__.py
@@ -1,3 +1,4 @@
+from typing import ChainMap
 import check50
 
 @check50.check()
@@ -5,6 +6,14 @@ def exists():
     """Verificando se existe o arquivo hello.py"""
     check50.exists('hello.py')
 
+
+@check50.check()
+def imprime_algo():
+    """"""
+    actual = check50.run("python3 hello.py").stdout()
+    if not actual:
+        help = 'parece que seu programa nao printa nada, você está invocando usa função?'
+    raise check50.Mismatch(help=help)
 
 @check50.check(exists)
 def prints_hello():
@@ -18,3 +27,5 @@ def prints_hello():
         if match(expected[:-1], actual):
             help = r"did you forget a newline ('\n') at the end of your printf string?"
         raise check50.Mismatch("hello, world\n", actual, help=help)
+
+check50.
