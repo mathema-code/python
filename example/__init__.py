4bd3a3eb283a3ea5014a7caa014375f6652706de
caiosalgado <caio.salgado@hotmail.com>
2021-10-29 22:11:02 -0300

    hello refeito

--- example/__init__.py
+++ example/__init__.py
@@ -5,13 +5,8 @@ def exists():
     """Verificando se existe o arquivo hello.py"""
     check50.exists('hello.py')
 
+
 @check50.check(exists)
-def hello_world():
-    """imprimindo "hello world" \n"""
-    check50.run("python3 hello.py").stdout("Hello, world!", regex=False).exit(0)
-
-
-@check50.check()
 def prints_hello():
     """prints "hello, world\\n" """
     from re import match
